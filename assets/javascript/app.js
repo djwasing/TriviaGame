e007fdaa808fef00c0fd6001bd54384a6205f22c
Don Wasinger <DonWasinger@Dons-MacBook-Pro.local>
2018-12-19 10:10:45 -0600

    questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,44 +7,45 @@ $(document).ready(function() {
     var time= 10;
     var timerOn = false;
     var intervalId;
+   
 
     var question= {
-        q1: "This is question 1.", 
-        q2: "This is question 2",
-        q3: "This is question 3",
-        q4: "This is question 4",
-        q5: "Q#5",
-        q6: "Q#6", 
-        q7: "Q#7",
-        q8: "Q#8",
-        q9: "Q#9",
-        q10: "Q#10"
+        q1: "What is the secret ingredient in Kevin's Famous Chili recipe?", 
+        q2: "How many pounds does Angela weigh?",
+        q3: "Which male character regularly uses the female restroom?",
+        q4: "What band poster is on the wall in Dwight's Daycare?",
+        q5: "Who from the office attends Pam's art show?",
+        q6: "How did Michael break the glass door in his condo?", 
+        q7: "Where was Jim and Pam's first kiss?",
+        q8: "What was the name of the show Michael appeared on as a child?",
+        q9: "How many push-ups can Michael do?",
+        q10: "What position was Kevin applying for when he was hired?"
     }
 
     var answer= {
-        a1: "This is answer 1.",
-        a2: "a#2",
-        a3: "a#3",
-        a4: "a#4",
-        a5: "A#5",
-        a6: "A#6",
-        a7: "A#7",
-        a8: "A#8",
-        a9: "A#9",
-        a10: "A#10"
+        a1: "Undercooked onions",
+        a2: "82",
+        a3: "Creed",
+        a4: "Insane Clown Posse",
+        a5: "Roy, Oscar, Michael",
+        a6: "Ran through it to catch the ice cream truck",
+        a7: "Chili's",
+        a8: "Fundle Bundle",
+        a9: "25, and 1 girl push-up",
+        a10: "Warehouse"
     }
 
     var options= {
-        o1: ["This", "That", "Other", "again"],
-        o2: ["1", "2", "3", "4"],
-        o3: ["1", "2", "3", "4"],
-        o4: ["1", "2", "3", "4"],
-        o5: ["1", "2", "3", "4"],
-        o6: ["1", "2", "3", "4"],
-        o7: ["1", "2", "3", "4"],
-        o8: ["1", "2", "3", "4"],
-        o9: ["1", "2", "3", "4"],
-        o10: ["1", "2", "3", "4"],
+        o1: ["Special sauce", "Undercooked  onions", "Fresh tomatoes", "Mexican beans"],
+        o2: ["85", "79", "90", "82"],
+        o3: ["Creed", "Gabe", "Ryan", "Toby"],
+        o4: ["U2", "Insane Clown Posse", "Scrantonicity", "The Rolling Stones"],
+        o5: ["Oscar, Andy, Jim", "Roy, Oscar, Michael", "Jim, Kelly, Ryan", "Creed, Meridith, Phyllis"],
+        o6: ["Ran through it to catch the ice cream truck", "Threw a dundie award at it", "Playing basketball", "Practicing his golf swing"],
+        o7: ["At Jim's desk after Casino Night", "At a gas station", "Chili's", "A hospital"],
+        o8: ["Barney and Friends", "Blues Clues", "Larry's Learning Time", "Fundle Bundle"],
+        o9: ["25, and 1 girl push-up", "26", "12", "21"],
+        o10: ["Sales", "Warehouse", "Human Resources", "Secretary"],
     }
 
     $("#start").on("click", function() {
@@ -61,7 +62,6 @@ $(document).ready(function() {
         time= 10;
         timerOn= true;
         runTime();
-        $("#results").text()
     }
 
     function runTime() {
@@ -73,7 +73,7 @@ $(document).ready(function() {
         time--;
         $("#timer").html(time);
         if (time == 0) {
-            alert("Out of time");
+            alert("Out of time. Press OK to continue game");
             incorrect++;       
             stopTime();
         }
@@ -82,6 +82,19 @@ $(document).ready(function() {
     function stopTime() {
         clearInterval(intervalId);
         $("#timer").html(time);
+        setTimeout(continueGame, 1000);
+    }
+
+    function continueGame() {
+        incorrect++;
+        time = 10;
+        $("#timer").html(time);
+        runTime();
+        //also, show next question
+    }
+
+    function newQuestion() {
+        var quizQuestion = 
     }
 
 
