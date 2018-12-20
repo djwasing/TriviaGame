7720a5e4d38b0da9382c4b715dec5accc6d6216b
Don Wasinger <DonWasinger@Dons-MacBook-Pro.local>
2018-12-19 21:35:59 -0600

    html version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,47 +7,8 @@ $(document).ready(function() {
     var time= 10;
     var timerOn = false;
     var intervalId;
+    var correctAnswer= "";
     $("#results").hide();
-   
-
-    questionArray= [
-        "What is the secret ingredient in Kevin's Famous Chili recipe?", 
-        "How many pounds does Angela weigh?",
-        "Which male character regularly uses the female restroom?",
-        "What band poster is on the wall in Dwight's Daycare?",
-        "Who from the office attends Pam's art show?",
-        "How did Michael break the glass door in his condo?", 
-        "Where was Jim and Pam's first kiss?",
-        "What was the name of the show Michael appeared on as a child?",
-        "How many push-ups can Michael do?",
-        "What position was Kevin applying for when he was hired?"
-    ]
-
-    var answerArray= [
-        "Undercooked onions",
-        "82",
-        "Creed",
-        "Insane Clown Posse",
-        "Roy, Oscar, Michael",
-        "Ran through it to catch the ice cream truck",
-        "Chili's",
-        "Fundle Bundle",
-        "25, and 1 girl push-up",
-        "Warehouse"
-    ]
-
-    var optionsArray= [
-        ["Special sauce", "Undercooked  onions", "Fresh tomatoes", "Mexican beans"],
-        ["85", "79", "90", "82"],
-        ["Creed", "Gabe", "Ryan", "Toby"],
-        ["U2", "Insane Clown Posse", "Scrantonicity", "The Rolling Stones"],
-        ["Oscar, Andy, Jim", "Roy, Oscar, Michael", "Jim, Kelly, Ryan", "Creed, Meridith, Phyllis"],
-        ["Ran through it to catch the ice cream truck", "Threw a dundie award at it", "Playing basketball", "Practicing his golf swing"],
-        ["At Jim's desk after Casino Night", "At a gas station", "Chili's", "A hospital"],
-        ["Barney and Friends", "Blues Clues", "Larry's Learning Time", "Fundle Bundle"],
-        ["25, and 1 girl push-up", "26", "12", "21"],
-        ["Sales", "Warehouse", "Human Resources", "Secretary"],
-    ]
 
     $("#startBtn").on("click", function() {
         $("#startBtn").hide();
@@ -57,13 +18,13 @@ $(document).ready(function() {
     function startGame() {
         correct= 0;
         incorrect= 0;
-        time= 10;
+        time= 60;
         timerOn= true;
-        runTime();
+        //runTime();      **commented out runtime so can work on site without dealing with clock
         newQuestion();
     }
 
-    function runTime() {
+    function runTime() {                                //start the clock running down by 1 sec. 
         clearInterval(intervalId);
         intervalId = setInterval(countDown, 1000);
     }
@@ -71,44 +32,44 @@ $(document).ready(function() {
     function countDown() {
         time--;
         $("#timer").html(time);
-        if (time == 0) {
+        if (time == 0) {                                            //if the user runs out of time, they get the question wrong and continue the game
             alert("Out of time. Press OK to continue game");
             incorrect++;       
             stopTime();
         }
     }
 
-    function stopTime() {
-        clearInterval(intervalId);
-        $("#timer").html(time);
-        setTimeout(continueGame, 1000);
+    function stopTime() {                       //stops the time when clock reaches 0, 
+        clearInterval(intervalId);      
+        $("#timer").html(time);                 //show time
+        setTimeout(continueGame, 1000);         //wait 1 sec, and continue the game
     }
 
-    function continueGame() {
-        time = 10;
+    function continueGame() {                           //if the user lets time expire, the game continues after showing the correct answer 
+        time = 10;                                         // resetting the clock
         $("#timer").html(time);
-        runTime();
-        console.log("incorrect: " + incorrect);
+        runTime();                                         //starting the time again
+        console.log("incorrect: " + incorrect);             
         newQuestion();
     }
 
-    function newQuestion() {
-        var quizQuestion = (questionArray);
-        $("#question").text(quizQuestion);
+    function newQuestion() {                        //select the first value in the object and display only it. 
+        $.each(question, function(key, value) {        // after user answers the question, display the ext question, until the end of the quiz. 
+            var currentQuestion =                       //assign the first question in the object to the currentQuestion VAR and display it
+            console.log(value);
+           
 
-        for (i = 0; i < optionsArray.length; i++) {
-            var btn = $("<button>");
-            btn.addClass("option");
-            btn.attr(optionsArray[i]);
-            btn.text(optionsArray);
-            $("#choices").append(btn);
-        }
+        })
+
+        
+
+
     }
 
 
     // To do list: 
     // 1. show answer options as stacked buttons
-    // 2. loop through questions and options
+    // 2. loop through questions and options, showing corresponding indecies
     // 3. build check answer function to check the selected answer against the correct answer when an answer button is clicked.
     // 4. Link .ajax API to show a gif after 1) time runs out, 2) each correct answer, 3) when game is Over. 
     
